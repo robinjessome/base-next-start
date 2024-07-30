940c8de5a17c4b11dddb64b2517cac2203523b8a
Robin Jessome <robinjessome@gmail.com>
2024-07-30 09:54:10 -0300

    WIP

--- src/components/helpers/LocaleSwitcher/LocaleSwitcher.stories.tsx
+++ src/components/helpers/LocaleSwitcher/LocaleSwitcher.stories.tsx
@@ -1,4 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react'
+import { NextIntlClientProvider } from 'next-intl'
 
 import { LocaleSwitcher } from '.'
 
@@ -8,6 +9,14 @@ export default {
   args: {
     text: 'Hello world!',
   },
+  // TODO figure out how to apply this Context globally
+  decorators: [
+    (Story) => (
+      <NextIntlClientProvider locale="en">
+        <Story />
+      </NextIntlClientProvider>
+    ),
+  ],
 } satisfies Meta<typeof LocaleSwitcher>
 
 type Story = StoryObj<typeof LocaleSwitcher>
