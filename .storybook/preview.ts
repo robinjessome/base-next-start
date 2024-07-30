56b58fbdcd5fdd8dc99f16771d2f8614c85ae3f8
Robin Jessome <robinjessome@gmail.com>
2024-07-30 10:00:53 -0300

    WIP

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -4,12 +4,6 @@ import type { Preview } from '@storybook/react'
 
 const preview: Preview = {
   parameters: {
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/i,
-      },
-    },
     viewport: {
       viewports: {
         'xs': {
